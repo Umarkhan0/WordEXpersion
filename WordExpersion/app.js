87c4d1d3f517c277ceac5bfdee8e874d185ae354
Umarkhan0 <125694026+Umarkhan0@users.noreply.github.com>
2023-05-17 19:19:17 +0845

    Add files via upload

---
+++ WordExpersion/app.js
@@ -0,0 +1,17 @@
+function changSize(){
+let changSize = document.getElementById("changSize").value;
+let text = document.getElementById("text");
+text.style.fontSize = changSize;
+}function changColor(){
+    let changColor =document.getElementById("changColor").value
+    let text =document.getElementById("text");
+    text.style.color = changColor;
+}function fontWaight(){
+    let fontWaight =document.getElementById("fontWaight").value
+    let text =document.getElementById("text");
+    text.style.fontWeight = fontWaight;
+}function changBackGroundColor(){
+    let changBackGroundColor =document.getElementById("changBackGroundColor").value
+    let body =document.body;
+    body.style.backgroundColor = changBackGroundColor;
+}
